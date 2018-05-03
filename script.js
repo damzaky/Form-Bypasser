acca3573d9b75568c6cc1e1ae1e2313c0d943756
damzaky <damzaky@gmail.com>
2018-05-03 20:57:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+var tx = "sesuatu";
+
 var t = document.querySelectorAll("input[type=text],textarea");
 var te = document.querySelectorAll("input[type=email]");
 var tp = document.querySelectorAll("input[type=password]");
-
+var r1,r2;
 for (var i=0; i<t.length; i++)
-t[i].value = "abc";
+t[i].value = tx;
 
 for (var i=0; i<te.length; i++)
-te[i].value = "abc";
+te[i].value = tx+"@a.com";
 
 for (var i=0; i<tp.length; i++)
-tp[i].value = "abc";
+tp[i].value = tx;
 
 var r = document.querySelectorAll('input[type=radio]');
 for (var i=0; i<r.length; i++)
 r[i].checked = true;
+for (var i=0; i<r.length; i++){
+r1=Math.floor((Math.random() * r.length) + 1); //randomizer
+r[r1].checked = true;
+}
 
 var c = document.querySelectorAll('input[type=checkbox]');
 for (var i=0; i<c.length; i++)
@@ -24,6 +30,10 @@ c[i].checked = true;
 var rg = document.querySelectorAll('*[class*="radio"]');
 for(i=0;i<rg.length;i++)
 rg[i].click();
+for (var i=0; i<rg.length; i++){
+r2=Math.floor((Math.random() * rg.length) + 1); //randomizer
+r[r2].checked = true;
+}
 var cg = document.querySelectorAll('*[class*="checkbox"]');
 for(i=0;i<cg.length;i++)
 cg[i].click();
